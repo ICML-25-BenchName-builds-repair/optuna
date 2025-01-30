19964e9018f6f3eecfea1c7cdf6133f4d9ab0870
ubuntu <ubuntu@ip-10-0-1-79>
2025-01-30 15:22:16 +0000

    waleko__claude-3-opus_f4f5__id_69

--- optuna/study/_multi_objective.py
+++ optuna/study/_multi_objective.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
 from collections import defaultdict
-from typing import List, Optional, Sequence
+from typing import List, Optional, Sequence 
 
 import numpy as np
 
 import optuna
 from optuna.study._study_direction import StudyDirection
-from optuna.trial import FrozenTrial, TrialState
+from optuna.trial import FrozenTrial, TrialState 
 
 
 def _get_pareto_front_trials_2d(
